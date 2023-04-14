2d4247f5188d7292d6bcf5bc6d71ae7dcdfff161
DmitriyODS <osipovskij.dima@yandex.ru>
2023-04-14 12:24:21 +0300

    Реализация бизнес логики заданных разделов
    * Клиенты
    * Заявки
    * Задачи
    * Списки

--- implementation/customers.go
+++ implementation/customers.go
@@ -3,16 +3,65 @@ package implementation
 import (
 	"context"
 	"gitlab.com/ddda/d-track/d-track-back/domain"
+	"gitlab.com/ddda/d-track/d-track-back/global"
+	"log"
 )
 
 func (bs *BasicService) GetListCustomers(ctx context.Context, fioFilter string, isArchive bool) ([]domain.Customer, error) {
-	return nil, nil
+	customers, err := bs.rep.SelectCustomers(ctx, fioFilter, isArchive)
+	if err != nil {
+		log.Println("GetListCustomers err:", err)
+		return nil, global.InternalServerErr
+	}
+
+	return customers, nil
 }
 
 func (bs *BasicService) GetCustomerByID(ctx context.Context, id uint64) (domain.Customer, error) {
-	return domain.Customer{}, nil
+	if id == 0 {
+		return domain.NewCustomer(0), global.BadRequestErr
+	}
+
+	customer, err := bs.rep.SelectCustomerByID(ctx, id)
+	if err != nil {
+		log.Println("GetCustomerByID err:", err)
+		return customer, global.InternalServerErr
+	}
+
+	if customer.ID == 0 {
+		return customer, global.DataNotFoundErr
+	}
+
+	return customer, nil
 }
 
 func (bs *BasicService) StoreCustomer(ctx context.Context, customer domain.Customer) (uint64, error) {
-	return 0, nil
+	// если id не задан - запрос на создание
+	if customer.ID == 0 {
+		// не прошли валидацию, отправляемся обратно
+		if !customer.ValidateFields(true) {
+			return 0, global.IncorrectValidFormErr
+		}
+
+		id, err := bs.rep.CreateCustomer(ctx, customer)
+		if err != nil || id == 0 {
+			log.Println("StoreCustomer create err:", err)
+			return 0, global.InternalServerErr
+		}
+
+		return id, nil
+	}
+
+	// проверка корректнности заполнения полей
+	if !customer.ValidateFields(false) {
+		return 0, global.IncorrectValidFormErr
+	}
+
+	id, err := bs.rep.UpdateCustomer(ctx, customer)
+	if err != nil || id == 0 {
+		log.Println("StoreCustomer update err:", err)
+		return 0, global.InternalServerErr
+	}
+
+	return id, nil
 }
--- implementation/tasks.go
+++ implementation/tasks.go
@@ -3,16 +3,65 @@ package implementation
 import (
 	"context"
 	"gitlab.com/ddda/d-track/d-track-back/domain"
+	"gitlab.com/ddda/d-track/d-track-back/global"
+	"log"
 )
 
 func (bs *BasicService) GetListTasks(ctx context.Context, numberFilter string, isArchive bool) ([]domain.Task, error) {
-	return nil, nil
+	tasks, err := bs.rep.SelectTasks(ctx, numberFilter, isArchive)
+	if err != nil {
+		log.Println("GetListTasks err:", err)
+		return nil, global.InternalServerErr
+	}
+
+	return tasks, nil
 }
 
 func (bs *BasicService) GetTaskByID(ctx context.Context, id uint64) (domain.Task, error) {
-	return domain.Task{}, nil
+	if id == 0 {
+		return domain.NewTask(0), global.BadRequestErr
+	}
+
+	task, err := bs.rep.SelectTaskByID(ctx, id)
+	if err != nil {
+		log.Println("GetTaskByID err:", err)
+		return task, global.InternalServerErr
+	}
+
+	if task.ID == 0 {
+		return task, global.DataNotFoundErr
+	}
+
+	return task, nil
 }
 
 func (bs *BasicService) StoreTask(ctx context.Context, task domain.Task) (uint64, error) {
-	return 0, nil
+	// если id не задан - запрос на создание
+	if task.ID == 0 {
+		// не прошли валидацию, отправляемся обратно
+		if !task.ValidateFields(true) {
+			return 0, global.IncorrectValidFormErr
+		}
+
+		id, err := bs.rep.CreateTask(ctx, task)
+		if err != nil || id == 0 {
+			log.Println("StoreTask create err:", err)
+			return 0, global.InternalServerErr
+		}
+
+		return id, nil
+	}
+
+	// проверка корректнности заполнения полей
+	if !task.ValidateFields(false) {
+		return 0, global.IncorrectValidFormErr
+	}
+
+	id, err := bs.rep.UpdateTask(ctx, task)
+	if err != nil || id == 0 {
+		log.Println("StoreTask update err:", err)
+		return 0, global.InternalServerErr
+	}
+
+	return id, nil
 }
